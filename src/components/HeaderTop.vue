acdc0b7c3930d206a4243d6b528a9d903be56819
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-26 14:30:55 +0200

    Add dinamic components header top

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,6 +1,20 @@
 <script>
 export default {
   name: "HeaderTop",
+  data() {
+    return {
+      callUs: "Call us for a Free Quote: 1.800.5555.6789",
+      icons: [
+        // icona left
+        ["fas", "phone-flip"],
+        // icone right
+        ["fab", "telegram"],
+        ["fab", "twitter"],
+        ["fab", "instagram"],
+        ["fab", "youtube"],
+      ],
+    };
+  },
 };
 </script>
 
@@ -8,14 +22,11 @@ export default {
   <div id="greenHeader" class="d-flex">
     <div class="container d-flex justify-content-between">
       <div class="left d-flex align-items-center">
-        <font-awesome-icon :icon="['fas', 'phone-flip']" />
-        <div class="ms-2">Call us for a Free Quote: 1.800.5555.6789</div>
+        <font-awesome-icon :icon="icons[0]" />
+        <div class="ms-2">{{ callUs }}</div>
       </div>
       <div class="right d-flex gap-2 align-items-center">
-        <font-awesome-icon :icon="['fab', 'telegram']" />
-        <font-awesome-icon :icon="['fab', 'twitter']" />
-        <font-awesome-icon :icon="['fab', 'instagram']" />
-        <font-awesome-icon :icon="['fab', 'youtube']" />
+        <font-awesome-icon v-for="icon in icons.splice(1)" :icon="icon" />
       </div>
     </div>
   </div>
@@ -24,12 +35,13 @@ export default {
 <style scoped lang="scss">
 @use "../styles/partials/_variables.scss" as *;
 
-#greenHeader{
+#greenHeader {
   height: 50px;
   background-color: $lima;
 }
 
-.left,.right{
+.left,
+.right {
   color: white;
 }
 </style>
