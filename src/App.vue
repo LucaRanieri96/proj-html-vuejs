7bcb5fc9ca7429c3502f5995f5608de7d0b955be
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-26 12:13:04 +0200

    Add header,main, footer components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script>
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
 
+export default {
+  components: {
+    SiteHeader,
+    SiteMain,
+    SiteFooter,
+  },
+  data() {
+    return{}
+  },
+  methods: {},
+};
 </script>
 
 <template>
- 
+ <SiteHeader></SiteHeader>
+ <SiteMain></SiteMain>
+ <SiteFooter></SiteFooter>
 </template>
 
 <style lang="scss">
