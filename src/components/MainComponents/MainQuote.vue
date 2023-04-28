d3aead7b819f31c54eba35d8380ab0b24463e94e
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-28 15:20:21 +0200

    Add simple animation in mainService on "appear"

--- src/components/MainComponents/MainQuote.vue
+++ src/components/MainComponents/MainQuote.vue
@@ -10,16 +10,23 @@ export default {
       <div class="row">
         <div class="col-6"></div>
         <div class="col-6">
-          <div class="card text-center">
-            <div class="card-body">
-              <span>WE REMOVE THE HEADACHE FROM MOVING</span>
-              <h3 class="card-title">Move With Ease</h3>
-              <p class="card-text">
-                With our super simple service we can take you from getting a quote, right through to being in your new home with all of your belongings securely packed and safely delivered, even in the rooms of your choice.
-              </p>
-              <button type="button" class="btn btn-primary">GET A FREE QUOTE NOW</button>
+          <Transition name="slide" appear>
+            <div class="card text-center">
+              <div class="card-body">
+                <span>WE REMOVE THE HEADACHE FROM MOVING</span>
+                <h3 class="card-title">Move With Ease</h3>
+                <p class="card-text">
+                  With our super simple service we can take you from getting a
+                  quote, right through to being in your new home with all of
+                  your belongings securely packed and safely delivered, even in
+                  the rooms of your choice.
+                </p>
+                <button type="button" class="btn btn-primary">
+                  GET A FREE QUOTE NOW
+                </button>
+              </div>
             </div>
-          </div>
+          </Transition>
         </div>
       </div>
     </div>
@@ -28,39 +35,49 @@ export default {
 
 <style lang="scss" scoped>
 @use "../../styles/partials/variables" as *;
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(150%);
+}
+
 #wallpaper {
   background-image: url(../../public/imgs/avada-movers-movewithease-background-new.jpg);
   background-size: cover;
   background-attachment: fixed;
 }
-.container{
+.container {
   padding: 10rem 0;
-  
 }
-.card{
+.card {
   border: none;
   border-radius: 0;
   background-color: $hippiegreen;
   padding: 1rem;
-  .card-body{
+  .card-body {
     padding: 40px;
     background-color: $lima;
     color: white;
-    span{
+    span {
       font-weight: 700;
     }
-    h3{
+    h3 {
       margin: 1rem 0;
       padding-bottom: 1.5rem;
       font-size: 50px;
       font-weight: 700;
       border-bottom: 1px solid $mantis;
     }
-    p{
+    p {
       font-size: 17px;
       line-height: 36px;
     }
-    button{
+    button {
       margin-top: 2rem;
       font-weight: 800;
       padding: 1rem 2.2rem !important;
--- src/components/MainComponents/MainService.vue
+++ src/components/MainComponents/MainService.vue
@@ -1,8 +1,8 @@
 <script>
 export default {
   name: "MainService",
-  data(){
-    return{
+  data() {
+    return {
       cards: [
         {
           img: "../../public/imgs/avada-movers-serviceonephoto-final.jpg",
@@ -23,30 +23,36 @@ export default {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
         },
       ],
-
-    }
-  }
+    };
+  },
 };
 </script>
 
 <template>
   <div id="background">
     <div
-      class="container d-flex align-items-center justify-content-center flex-column">
+      class="container d-flex align-items-center justify-content-center flex-column"
+    >
+    <transition name="slide" appear>
       <p id="title">WE ARE A PROFESSIONAL</p>
+    </transition>
+    <transition name="slide" appear>
       <h2 class="mb-5">Full-Service Solution</h2>
+    </transition>
       <div class="row">
         <div v-for="card in cards" class="col">
-          <div class="card text-center">
-            <img class="card-img-top" :src="card.img" alt="Card image cap" />
-            <div class="card-body">
-              <h3 class="card-title">{{card.title}}</h3>
-              <p>
-                {{card.paragraph}}
-              </p>
-              <button type="button" class="btn btn-primary">READ MORE</button>
+          <Transition name="v" appear>
+            <div class="card text-center" :key="card.id">
+              <img class="card-img-top" :src="card.img" alt="Card image cap" />
+              <div class="card-body">
+                <h3 class="card-title">{{ card.title }}</h3>
+                <p>
+                  {{ card.paragraph }}
+                </p>
+                <button type="button" class="btn btn-primary">READ MORE</button>
+              </div>
             </div>
-          </div>
+          </Transition>
         </div>
       </div>
     </div>
@@ -55,7 +61,28 @@ export default {
 
 <style lang="scss" scoped>
 @use "../../styles/partials/variables" as *;
-#title{
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 1.5s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-50%);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+#title {
   font-weight: 600;
 }
 #background {
@@ -67,33 +94,32 @@ export default {
   border-radius: 0;
   background-color: $mercury;
   padding: 1.5rem;
-  img{
+  img {
     margin-bottom: 1.5rem;
     transition: all 0.3s;
     border-radius: 0;
   }
-  img:hover{
+  img:hover {
     cursor: pointer;
     scale: 105%;
   }
-  .card-body{
+  .card-body {
     padding: 2rem;
     background-color: $light;
   }
-
 }
 .container {
   padding: 8rem 0;
   height: 100%;
-  h3{
+  h3 {
     font-weight: 700;
     color: $denim;
   }
-  p{
+  p {
     font-weight: 200;
     line-height: 32px;
   }
-  button{
+  button {
     font-weight: 600;
   }
 }
